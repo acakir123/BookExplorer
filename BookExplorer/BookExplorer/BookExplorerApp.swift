2352cae1ea9adad9a21f45e7db53fe4acf078d96
Ahmed Cakir <ayahyacakir@gmail.com>
2025-10-25 15:32:43 -0500

    Initial code setup

---
+++ BookExplorer/BookExplorer/BookExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookExplorerApp.swift
+//  BookExplorer
+//
+//  Created by Ahmet Cakir on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BookExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
