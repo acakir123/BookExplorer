252e22ec27fe6d301230b1ead27330d19e4ff766
Ahmed Cakir <ayahyacakir@gmail.com>
2025-10-27 14:32:04 -0500

    Removed color extension

--- BookExplorer/BookExplorer/Models/BookItem.swift
+++ BookExplorer/BookExplorer/Models/BookItem.swift
@@ -40,21 +40,6 @@ final class BookItem {
         }
 }
 
-extension Color {
-    init(hex: String) {
-        let scanner = Scanner(string: hex)
-        var hexNumber: UInt64 = 0
-        if scanner.scanHexInt64(&hexNumber) {
-            let r = Double((hexNumber & 0xff0000) >> 16) / 255
-            let g = Double((hexNumber & 0x00ff00) >> 8) / 255
-            let b = Double(hexNumber & 0x0000ff) / 255
-            self.init(red: r, green: g, blue: b)
-            return
-        }
-        self = .black
-    }
-}
-
 extension BookItem { // Extension to help with previews
     @MainActor
     
